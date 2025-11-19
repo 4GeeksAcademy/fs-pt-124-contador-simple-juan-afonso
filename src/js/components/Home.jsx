75572cd2d7fa6b24cd33fbf740cb07c1dfedb575
afo32 <63719917+afo32@users.noreply.github.com>
2025-11-19 19:37:49 +0000

    extra added

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -7,20 +7,29 @@ import Digit from "./Digit";
 import { faClock } from "@fortawesome/free-regular-svg-icons";
 
 //create your first component
-const Home = ({timer}) => {
+const Home = ({timer, onStart, onCountdown, onStop, onReset, onResume}) => {
 
-	const digits = String(timer % 10000).padStart(6, "0").split("");
+	const digits = String(timer).padStart(6, "0").split("");
 
 	return (
-		<div className="d-flex justify-content-center align-items-center bg-dark text-white p-3">
-      <div className="p-3 m-1 digit-square rounded">
-        <FontAwesomeIcon icon={faClock} />
-		
+		<div className="text-center">
+      <div className="d-flex justify-content-center align-items-center bg-dark text-white p-3 mb-3">
+        <div className="digit-square rounded text-white">
+          <FontAwesomeIcon icon={faClock} />
+        </div>
+        {digits.map((d, index) => (
+          <Digit key={index} digit={d} />
+        ))}
       </div>
-	  
-      {digits.map((d, index) => (
-        <Digit key={index} digit={d} />
-      ))}
+
+
+      <div className="btn-group">
+        <button className="btn btn-dark m-3 rounded" onClick={onStart}>Start</button>
+        <button className="btn btn-dark m-3 rounded" onClick={onCountdown}>Countdown</button>
+        <button className="btn btn-dark m-3 rounded" onClick={onStop}>Stop</button>
+        <button className="btn btn-dark m-3 rounded" onClick={onReset}>Reset</button>
+        <button className="btn btn-dark m-3 rounded" onClick={onResume}>Resume</button>
+      </div>
     </div>
 	);
 };
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -12,14 +12,69 @@ import '../styles/index.css'
 // components
 import Home from './components/Home';
 
-let timer = 0
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
-setInterval(() => {
+
+let timer = 0;
+let intervalId = null;
+const startNumber = 999999;
+
+function render() {
   root.render(
-      <React.StrictMode>
-            <Home timer={timer} />
-                </React.StrictMode>,
-                  )
-                    timer++
-                    }, 1000)
+    <React.StrictMode>
+      <Home
+        timer={timer}
+        onStart={startCounter}
+        onCountdown={startCountdown}
+        onStop={stopCounter}
+        onReset={resetCounter}
+        onResume={resumeCounter}
+      />
+    </React.StrictMode>
+  );
+}
+
+function startCounter() {
+  if (!intervalId) {
+    intervalId = setInterval(() => {
+      timer++;
+      render();
+    }, 1000);
+  }
+}
+
+function startCountdown() {
+  timer = startNumber;
+  if (!intervalId) {
+    intervalId = setInterval(() => {
+      if (timer > 0) {
+        timer--;
+        render();
+      } else {
+        stopCounter();
+      }
+    }, 1000);
+  }
+}
+
+function stopCounter() {
+  clearInterval(intervalId);
+  intervalId = null;
+}
+
+function resetCounter() {
+  stopCounter();
+  timer = 0;
+  render();
+}
+
+function resumeCounter() {
+  if (!intervalId) {
+    intervalId = setInterval(() => {
+      timer++;
+      render();
+    }, 1000);
+  }
+}
+
+render();
+
